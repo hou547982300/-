82587779ad68a7eae879fad3e89d9ed789124c61
hou547982300 <547982300@qq.com>
2018-10-31 22:22:29 +0800

    some change

--- src/js/bid_request_list.js
+++ src/js/bid_request_list.js
@@ -1,8 +1,10 @@
 $(function(){
 
-    //这是借款列表的动态渲染
+    //这是借款列表的动态渲染   dddd
    $.get("./api/borrow_list.php",function(data){
     var htmlStr =$("#borrowTmpl").tmpl(data);
+
+    s
     $("#bidrequestlist").html(htmlStr);
    },'json')
 })
